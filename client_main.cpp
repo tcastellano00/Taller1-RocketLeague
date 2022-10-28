5012f03e4c18e90f12d2f7465ad8d8f0c1791436
Tomas Martin <tmartin@fi.uba.ar>
2022-10-28 18:56:55 -0300

    Primero archivos del cliente y makefile

---
+++ client_main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <exception>
+
+int main(int argc, char *argv[]) { try {
+    int ret = 1;
+
+    std::cout << "Test" << "\n";
+
+    ret = 0;
+    return ret;
+} catch (const std::exception& err) {
+    std::cerr
+        << "Something went wrong and an exception was caught: "
+        << err.what()
+        << "\n";
+    return 1;
+} catch (...) {
+    std::cerr << "Something went wrong and an unknown exception was caught.\n";
+    return 1;
+} }
+
