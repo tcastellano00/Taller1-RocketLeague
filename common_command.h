45906c89bbc71c6c0a07ea1534d61faeb257ef59
feburs <fernando.bursztyn@gmail.com>
2022-10-30 01:40:02 -0300

    archivo sin guardar del push anterior

--- common_command.h
+++ common_command.h
@@ -9,12 +9,10 @@
 //manejar dicha informacion con mas facilidad.
 class Command {
     std::string type_command;
-    //std::string informacion_comando;
 
     public:
     Command(const std::string &type_command);
     
     std::string get_type_command();
-    //std::string obtener_informacion_del_comando();
 };
 #endif
