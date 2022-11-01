766d716d882cf1b6fb9a6e39c2d734bbbe9b10fc
MarcosC <mcouttulenc@fi.uba.ar>
2022-11-01 01:35:59 -0300

    implemento el receiver

--- Client/ThreadReceiver.cpp
+++ Client/ThreadReceiver.cpp
@@ -1,10 +1,20 @@
 #include "ThreadReceiver.h"
 #include <iostream>
+#include "../Common/Protocol.h"
 
-ThreadReceiver::ThreadReceiver(GameStatusMonitor& newGameStatusMonitor):gameStatusMonitor(newGameStatusMonitor){}
+ThreadReceiver::ThreadReceiver(Socket& cnct, GameStatusMonitor& newGameStatusMonitor): gameStatusMonitor(newGameStatusMonitor), connection(cnct){}
 
 void ThreadReceiver::run() {
     std::cout << "Im running receiver thread" << std::endl;
+    Protocol protocol(connection);
+    while (!gameStatusMonitor.gameIsClosed()) {
+        std::string response = protocol.reciveMessage();
+        gameStatusMonitor.statusUpdate(response);
+        if (response == "close") {
+            gameStatusMonitor.setClose();
+        }
+    }
+
 }
 
 ThreadReceiver::~ThreadReceiver() {
