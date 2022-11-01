bf91f9fc7f9b809ec8677d07972bcb3ff98ad178
MarcosC <mcouttulenc@fi.uba.ar>
2022-11-01 01:36:40 -0300

    le agrego un metodo al gamestatus

--- Client/GameStatusMonitor.cpp
+++ Client/GameStatusMonitor.cpp
@@ -19,3 +19,7 @@ void GameStatusMonitor::statusUpdate(std::string newState){
     std::lock_guard<std::mutex> lock(gameMutex);
     gamestatus.statusUpdate(newState);
 }
+
+void GameStatusMonitor::setClose() {
+    gamestatus.setClose();
+}
