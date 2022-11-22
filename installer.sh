ad65d17830246a834bb7d5ecfd54419bfedc5bf3
Tomas Martin <tmartin@fi.uba.ar>
2022-11-22 17:47:55 -0300

    Borro archivos generados por el instalador al reinstalar

--- installer.sh
+++ installer.sh
@@ -3,6 +3,11 @@
 # exit when any command fails
 set -e
 
+sudo rm -rf /usr/local/bin/rocket_league
+sudo rm -rf /usr/local/bin/rocket_league-server
+sudo rm -rf /usr/local/bin/librocket_league-common.so
+sudo rm -rf /usr/local/bin/assets
+
 cd build
 sudo make install -j4
 cd ..
