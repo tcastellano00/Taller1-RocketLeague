c69b0dec1ebbf68e0fbaa5f078b4d4206ca8fa7d
MarcosC <mcouttulenc@fi.uba.ar>
2022-12-03 02:39:12 -0300

    completamos el yaml del server y los getters correspondientes del serverConfig
    Co-authored-by: ferburs <ferburs@users.noreply.github.com>

--- Server/Config/ServerConfig.cpp
+++ Server/Config/ServerConfig.cpp
@@ -16,6 +16,107 @@ ServerConfig* ServerConfig::getInstance() {
     return serverConfig;
 }
 
-int ServerConfig::getGravity() {
-    return getInstance()->rootNode["GRAVITY"].as<int>();
+float ServerConfig::getGravity() {
+    return getInstance()->rootNode["GRAVITY"].as<float>();
+}
+
+float ServerConfig::getGameTime() {
+    return getInstance()->rootNode["GAME_TIME"].as<float>();
+}
+
+float ServerConfig::getExtraTime() {
+    return getInstance()->rootNode["EXTRA_TIME"].as<float>();
+}
+
+float ServerConfig::getReplayTime() {
+    return getInstance()->rootNode["REPLAY_TIME"].as<float>();
+}
+
+float ServerConfig::getFlipImpulse() {
+    return getInstance()->rootNode["FLIP_IMPULSE"].as<float>();
+}
+
+float ServerConfig::getFlipAngularVel() {
+    return getInstance()->rootNode["FLIP_ANGULAR_VEL"].as<float>();
+}
+
+float ServerConfig::getCarHalfWidth() {
+    return getInstance()->rootNode["CAR_HALF_WIDTH"].as<float>();
+}
+
+float ServerConfig::getCarHalfHeight() {
+    return getInstance()->rootNode["CAR_HALF_HEIGHT"].as<float>();
+}
+
+float ServerConfig::getTurboForce() {
+    return getInstance()->rootNode["TURBO_FORCE"].as<float>();
+}
+
+float ServerConfig::getJumpImpulse() {
+    return getInstance()->rootNode["JUMP_IMPULSE"].as<float>();
+}
+
+float ServerConfig::getMovementForce() {
+    return getInstance()->rootNode["MOVEMENT_FORCE"].as<float>();
+}
+
+float ServerConfig::getCarFriction() {
+    return getInstance()->rootNode["CAR_FRICTION"].as<float>();
+}
+
+float ServerConfig::getFrontSensorHalfWidth() {
+    return getInstance()->rootNode["FRONT_SENSOR_HALF_WIDTH"].as<float>();
+}
+
+float ServerConfig::getBottomSensorHalfHeigth() {
+    return getInstance()->rootNode["BOTTOM_SENSOR_HALF_HEIGHT"].as<float>();
+}
+
+
+
+float ServerConfig::getWallWidth() {
+    return getInstance()->rootNode["WALL_WIDTH"].as<float>();
+}
+
+float ServerConfig::getGroundFriction() {
+    return getInstance()->rootNode["GROUND_FRICTION"].as<float>();
+}
+
+
+float ServerConfig::getFieldHalfWidth() {
+    return getInstance()->rootNode["FIELD_HALF_WIDTH"].as<float>();
+}
+
+float ServerConfig::getFieldHeight() {
+    return getInstance()->rootNode["FIELD_HEIGHT"].as<float>();
+}
+
+
+float ServerConfig::getGoalTopHalfWidth() {
+    return getInstance()->rootNode["GOAL_TOP_HALF_WIDTH"].as<float>();
+}
+
+float ServerConfig::getGoalTopHalfHeight() {
+    return getInstance()->rootNode["GOAL_TOP_HALF_HEIGHT"].as<float>();
+}
+
+float ServerConfig::getBallRadius() {
+    return getInstance()->rootNode["BALL_RADIUS"].as<float>();
+}
+
+
+float ServerConfig::getRedShotImpulse() {
+    return getInstance()->rootNode["RED_SHOT_IMPULSE"].as<float>();
+}
+
+float ServerConfig::getGoldShotImpulse() {
+    return getInstance()->rootNode["GOLD_SHOT_IMPULSE"].as<float>();
+}
+
+float ServerConfig::getPurpleShotImpulse() {
+    return getInstance()->rootNode["PURPLE_SHOT_IMPULSE"].as<float>();
+}
+
+float ServerConfig::getSpecialShotTime() {
+    return getInstance()->rootNode["SPECIAL_SHOT_TIME"].as<float>();
 }
